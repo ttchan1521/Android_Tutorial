331cc75cb0cb66ad1eb3890a2bf5c3d063019a5b
ttchan1521 <tranthutrang15012001@gmail.com>
2022-03-19 22:08:41 +0700

    add fun random

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,9 +12,22 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "4"
+            rollDice()
         }
 
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(val numSides: Int) {
+
+    fun roll() : Int {
+        return (1..numSides).random()
     }
 }
