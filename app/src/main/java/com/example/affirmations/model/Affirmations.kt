cf6e43994906883e30e8de677c300039880390a0
ttchan1521 <tranthutrang15012001@gmail.com>
2022-04-10 17:20:14 +0700

    add image

--- app/src/main/java/com/example/affirmations/model/Affirmations.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmations.kt
@@ -1,4 +1,8 @@
 package com.example.affirmations.model
 
-data class Affirmations (val stringResouceId: Int){
-}
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmations (
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int)
