939764498562dc4c042d75cf95a425cc08f556b8
ttchan1521 <tranthutrang15012001@gmail.com>
2022-04-10 23:17:21 +0700

    instrumentation test

---
+++ app/src/androidTest/java/com/example/affirmations/AffirmationsListTests.kt
@@ -0,0 +1,32 @@
+package com.example.affirmations
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions
+import android.support.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AffirmationsListTests {
+
+    @get:Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun scroll_to_item() {
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions
+                .scrollTo<android.support.v7.widget.RecyclerView.ViewHolder>(
+                    withText(R.string.affirmation10)
+                )
+        )
+
+        onView(withText(R.string.affirmation10))
+            .check(matches(isDisplayed())
+            )
+    }
+}
